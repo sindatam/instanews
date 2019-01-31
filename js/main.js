66a0312197568f25f97994c77e28f5fac9f4d9a9
sindatam <lilli.sinda,tam@gmail.com>
2019-01-31 15:05:32 +0000

    mobile version for instanews

--- js/main.js
+++ js/main.js
@@ -1,36 +1,36 @@
-$('#dropdown-menu').on('change', function() {
- $.ajax({
-    method: 'GET',
-    url: 'https://api.nytimes.com/svc/topstories/v2/'+$('#dropdown-menu').val()+'.json?api-key=0751ffff01d7a70710354972fa0ad4a9'
- })
- .done(function(data) {
-// loop in the function in order to get 12 different articles in the dropdown menu
-    $('p').empty(); 
-  for (i = 0; i <=11; i++) {
-   console.log(data.results);
-   var description = data.results[i].abstract
-    var imageUrl= data.results[i].multimedia[4].url
-   var articleLink = data.results[i].url 
-    console.log(description)
-    console.log(imageUrl)
-    console.log(articleLink)
+$('#dropdown-menu').on('change', function () {
+   $.ajax({
+      method: 'GET',
+      url: 'https://api.nytimes.com/svc/topstories/v2/' + $('#dropdown-menu').val() + '.json?api-key=0751ffff01d7a70710354972fa0ad4a9'
+   })
+      .done(function (data) {
+         // loop in the function in order to get 12 different articles in the dropdown menu
+         $('p').empty();
+         for (i = 0; i <= 11; i++) {
+            console.log(data.results);
+            var description = data.results[i].abstract
+            var imageUrl = data.results[i].multimedia[4].url
+            var articleLink = data.results[i].url
+            console.log(description)
+            console.log(imageUrl)
+            console.log(articleLink)
 
- var htmlString = '<a class="textColor" href="'+articleLink+'"><div class="articleContainer"><h2>' + description + '</h2></div></a>'
-  console.log(htmlString);
+            // click on the description to get to the url link
 
-  $('p').append(htmlString);
-  $('.articleContainer').last().css('background-image', 'url(' + imageUrl+')');
+            // Telling html how the articles shall appear.
+            // $('.news-area').append( - targets empty div in HTML and appends data to it 
+            var htmlString = '<a class="textColor" href="' + articleLink + '"><div class="articleContainer"><h1>' + description + '</h1></div></a>'
+            // console.log(htmlString);
 
-
-  }
-
-
-    });
+            $('.news-area').append(htmlString);
+            $('.articleContainer').last().css('background-image', 'url(' + imageUrl + ')');
 
 
-       
- });
+         }
 
- 
- 
 
+      });
+
+
+
+});
